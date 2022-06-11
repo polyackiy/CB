1190adc87d36bc1aceb1d4f61b6a8c3afea8488f
BravoTangoCharlie <polyackiy@gmail.com>
2022-06-11 19:48:09 +0300

    Add login button

--- client/components/Header.js
+++ client/components/Header.js
@@ -1,32 +1,52 @@
-import {Menu} from "semantic-ui-react";
+import { Button, Menu } from "semantic-ui-react";
 import Link from "next/link";
+import { useState } from "react";
 
 const Header = () => {
-    return (
-        <Menu style={{marginTop: "20px"}}>
-            <Link href="/">
-                <Menu.Item
-                    name='main'
-                >
-                    Главная
-                </Menu.Item>
-            </Link>
-            <Link href="/addContact">
-                <Menu.Item
-                    name='save'
-                >
-                    Записать контакт
-                </Menu.Item>
-            </Link>
-            <Link href="/showContact">
-                <Menu.Item
-                    name='openContact'
-                >
-                    Посмотреть контакт
-                </Menu.Item>
-            </Link>
-        </Menu>
-    );
+  const [currentAccount, setCurrentAccount] = useState();
+
+  const handleLoginClick = async () => {
+    const { ethereum } = window;
+    if (!ethereum) {
+      alert("Не найден WEB3 кошелек (например Metamask)");
+    }
+
+    try {
+      const accounts = await ethereum.request({
+        method: "eth_requestAccounts",
+      });
+      setCurrentAccount(accounts[0]);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  return (
+    <Menu style={{ marginTop: "20px" }}>
+      <Link href="/">
+        <Menu.Item name="main">Главная</Menu.Item>
+      </Link>
+      <Link href="/addContact">
+        <Menu.Item name="save">Записать контакт</Menu.Item>
+      </Link>
+      <Link href="/showContact">
+        <Menu.Item name="openContact">Посмотреть контакт</Menu.Item>
+      </Link>
+      <Menu.Item position="right">
+        {!currentAccount ? (
+          <Button primary onClick={handleLoginClick}>
+            Вход
+          </Button>
+        ) : (
+          <Link href="/user">
+            <Button primary onClick={handleLoginClick}>
+              {currentAccount}
+            </Button>
+          </Link>
+        )}
+      </Menu.Item>
+    </Menu>
+  );
 };
 
 export default Header;
---
+++ client/pages/user.js
@@ -0,0 +1,7 @@
+import Layout from "../components/Layout";
+
+const UserPage = () => {
+  return <Layout>User Page</Layout>;
+};
+
+export default UserPage;
